102ef91b9dd93fca58de24ea5e233cdc76937643
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2022-11-28 11:57:21 +0900

    Firebase連携とユーザー作成

--- lib/main.dart
+++ lib/main.dart
@@ -3,28 +3,54 @@ import 'package:flutter/material.dart';
 import 'package:matching_app/constants/routes.dart' as routes;
 import 'package:matching_app/details/rounded_button.dart';
 import 'package:matching_app/firebase_options.dart';
-import 'package:matching_app/views/login_page.dart';
+import 'package:matching_app/models/main_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Matching-App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginPage(),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final MainModel mainModel = ref.watch(mainProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Title')
+        ),
+        body: Column(
+          children: [
+            Text('Hello')
+          ],
+        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => mainModel.createUser(context: context),
+        tooltip: 'Signup',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
--- lib/models/main_model.dart
+++ lib/models/main_model.dart
@@ -1,10 +1,28 @@
 //flutter
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 //packages
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:matching_app/domain/firestore_user/firestore_user.dart';
+import 'package:uuid/uuid.dart';
 
 final mainProvider = ChangeNotifierProvider((ref) => MainModel());
 
 class MainModel extends ChangeNotifier {
-  
+  Future<void> createUser({required BuildContext context}) async {
+    final uuid = Uuid();
+    final String v4 = uuid.v4();
+    final Timestamp now = Timestamp.now();
+    final FirestoreUser firestoreUser = FirestoreUser(
+      createdAt: now,
+      uid: v4,
+      updatedAt: now,
+      userName: "Alice",
+    );
+    final Map<String,dynamic> userData = firestoreUser.toJson();
+    await FirebaseFirestore.instance.collection("users").doc(v4).set(userData);
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text('ユーザーが作成できました')));
+    notifyListeners();
+  }
 }
