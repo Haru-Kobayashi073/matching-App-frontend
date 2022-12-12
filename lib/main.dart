b022d3c4662625809fc6f4d2b7992d56ff0b9e5b
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2022-12-12 10:31:17 +0900

    ホームのレイアウト調整

--- lib/main.dart
+++ lib/main.dart
@@ -30,12 +30,28 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final User? onceUser = FirebaseAuth.instance.currentUser;
+    const int _0xff7a9beePrimaryValue = 0xff7a9bee;
+    const MaterialColor customSwatch = MaterialColor(
+      _0xff7a9beePrimaryValue,
+      <int, Color>{
+        50: Color(0xFFE8EAF6),
+        100: Color(0xFFC5CAE9),
+        200: Color(0xFF9FA8DA),
+        300: Color(0xFF7986CB),
+        400: Color(0xFF5C6BC0),
+        500: Color(_0xff7a9beePrimaryValue),
+        600: Color(0xFF3949AB),
+        700: Color(0xFF303F9F),
+        800: Color(0xFF283593),
+        900: Color(0xFF1A237E),
+      },
+    );
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Matching-App',
       theme: ThemeData(
-        primarySwatch: Colors.lightGreen,
+        primarySwatch: customSwatch,
       ),
       home: onceUser == null ? LoginPage() : MyHomePage(),
     );
--- lib/views/main/home_screen.dart
+++ lib/views/main/home_screen.dart
@@ -1,16 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:matching_app/details/user_image.dart';
+import 'package:decorated_icon/decorated_icon.dart';
+import 'package:matching_app/main.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final maxWidth = MediaQuery.of(context).size.width;
+    final maxheight =  MediaQuery.of(context).size.height;
+
+
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: const Icon(Icons.search),
       ),
-      body: Container(),
+      body: Center(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            const SizedBox(height: 30,),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: const Icon(Icons.account_circle_rounded,
+              size: 120,
+              ),
+          ),
+            const Text(
+              'userName',
+              style: TextStyle(fontSize: 24,fontWeight: FontWeight.w600),
+              ),
+              SizedBox(height: maxheight * 0.05,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: const [
+                      Text('0',
+                      style: TextStyle(fontSize: 20),),
+                      Text('フレンド',
+                      style: TextStyle(fontSize: 12),),
+                    ],
+                  ),
+                  Column(
+                    children: const [
+                      Text('0',
+                      style: TextStyle(fontSize: 20),),
+                      Text('参加サークル',
+                      style: TextStyle(fontSize: 12),),
+                    ],
+                  ),
+                ],
+              )
+          ],
+        ),
+      ),
     );
   }
 }
